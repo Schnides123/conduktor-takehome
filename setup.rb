cd3ed1f3e6bbb4bac07a2a1448ce351cb63520f6
Alex Schneider <aas339@cornell.edu>
2025-02-19 22:43:17 -0500

    fixed setup

--- setup.rb
+++ setup.rb
@@ -20,43 +20,58 @@ module OS
     end
   end  
 
+def run_setup
 
-`cd docker-compose`
-`docker-compose up -d`
-output = `rpk version`
-unless output.starts_with? "rpk version"
-    if OS.mac?
-        `brew install redpanda-data/tap/redpanda`
-    elsif OS.linux?
-        flag = false
-        while not flag
-            puts "installing redpanda CLI... type x86 if you're on an x86/x64 machine, or arm for ARM, or exit to cancel."
+    puts `docker-compose -f docker-compose/docker-compose.yml up -d `
+    output = `rpk --version`
+    puts output
+    unless output.start_with? "rpk version"
+        if OS.mac?
+            puts "no redpanda CLI found. Install with brew? (y/n)"
             input = gets.chomp.downcase
-            if input == "x86":
-                flag = true
-                `curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-amd64.zip && 
-                mkdir -p ~/.local/bin &&
-                export PATH="~/.local/bin:$PATH" &&
-                unzip rpk-linux-amd64.zip -d ~/.local/bin/`
-            elsif input == "amd":
-                flag = true
-                `curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-arm64.zip &&
-                mkdir -p ~/.local/bin &&
-                export PATH="~/.local/bin:$PATH" &&
-                unzip rpk-linux-arm64.zip -d ~/.local/bin/`
-            elsif input == "exit":
+            if input == "y"
+                `brew install redpanda-data/tap/redpanda`
+            else
                 return
             end
+        elsif OS.linux?
+            puts "no redpanda CLI found. Install? (y/n)"
+            input = gets.chomp.downcase
+            if input == "y"
+                flag = false
+                while not flag
+                    puts "installing redpanda CLI... type x86 if you're on an x86/x64 machine, or arm for ARM, or exit to cancel."
+                    input = gets.chomp.downcase
+                    if input == "x86"
+                        flag = true
+                        puts `curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-amd64.zip && 
+                        mkdir -p ~/.local/bin &&
+                        export PATH="~/.local/bin:$PATH" &&
+                        unzip rpk-linux-amd64.zip -d ~/.local/bin/`
+                    elsif input == "amd"
+                        flag = true
+                        puts `curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-arm64.zip &&
+                        mkdir -p ~/.local/bin &&
+                        export PATH="~/.local/bin:$PATH" &&
+                        unzip rpk-linux-arm64.zip -d ~/.local/bin/`
+                    elsif input == "exit"
+                        return
+                    end
+                end
+            else
+                return
+            end
+        else
+            puts "you're probably using windows or some hipster OS, please install the RPK CLI and rerun this"
+            return
         end
-        `rpk profile create quickstart --from-profile rpk-profile.yaml`
-        output = `rpk topic list`
-    else
-        puts("you're probably using windows or some hipster OS, please install the RPK CLI and rerun this")
+    end
+    puts `rpk profile create quickstart --from-profile docker-compose/rpk-profile.yaml`
+    output = `rpk topic list`
+    puts output
+    unless output.include? "people"
+        puts `rpk topic create people -p 3`
     end
 end
-output = `rpk topic list`
-unless output.include? "people"
-    `rpk topic create people -p 3`
-end
-`cd ../`
-`maven clean install`
+
+run_setup
